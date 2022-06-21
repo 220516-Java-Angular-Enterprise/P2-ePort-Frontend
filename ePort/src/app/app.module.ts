16cd436581634f90c237e1073f0e2fa3a06b7107
Joseph Choi <joseph375@revature.net>
2022-06-21 16:04:46 -0400

    Added routing. Added global background. Added login component.

--- ePort/src/app/app.module.ts
+++ ePort/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
+import { AppRouterModule } from './app-router/app-router.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
