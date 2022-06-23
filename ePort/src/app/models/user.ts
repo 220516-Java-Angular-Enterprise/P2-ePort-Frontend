bb1e5c2d8fdd575dc5b18160f57a8a31fa168f94
mb_q <mbquercioli@gmail.com>
2022-06-23 09:23:10 -0600

    updated user service

--- ePort/src/app/models/user.ts
+++ ePort/src/app/models/user.ts
@@ -3,6 +3,9 @@ export class User {
     constructor(
         public id: number,
         public name: string,
-        public codename: string
+        public codename: string,
+        public password: string,
+        public shippingAddress: string,
+        public email: string
     ) { }
 }
